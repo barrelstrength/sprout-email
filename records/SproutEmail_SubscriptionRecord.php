649d47d6e4543c071b3a709c9aeb701cdd8ed8c1
Ben Parizek <ben@barrelstrengthdesign.com>
2014-06-07 23:21:27 -0700

    Add initial support for subscribe/unsubscribe functionality

---
+++ records/SproutEmail_SubscriptionRecord.php
@@ -0,0 +1,32 @@
+<?php
+namespace Craft;
+
+class SproutEmail_SubscriptionRecord extends BaseRecord
+{	
+	/**
+	 * Return table name corresponding to this record
+	 * @return string
+	 */
+	public function getTableName()
+	{
+		return 'sproutemail_subscriptions';
+	}
+
+	/**
+	 * These have to be explicitly defined in order for the plugin to install
+	 * @return array
+	 */
+    public function defineAttributes()
+    {
+        return array();
+    }
+    
+
+    public function defineRelations()
+    {
+        return array(
+            'user' => array(static::BELONGS_TO, 'UserRecord', 'required' => true, 'onDelete' => static::CASCADE),
+            'element' => array(static::BELONGS_TO, 'ElementRecord', 'required' => true, 'onDelete' => static::CASCADE),
+        );
+    }
+}
