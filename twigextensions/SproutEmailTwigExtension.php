189e0571e6ffc2075b915370aca9560ff560a552
Ben Parizek <ben@barrelstrengthdesign.com>
2014-08-11 23:33:22 -0700

    inlineCss and wordwrap Twig filters - Take 1

--- twigextensions/SproutEmailTwigExtension.php
+++ twigextensions/SproutEmailTwigExtension.php
@@ -17,8 +17,6 @@ class SproutEmailTwigExtension extends Twig_Extension
 		return array (
 			'inlineCss' => new Twig_Filter_Method( $this, 'inlineCssFilter',  array('is_safe' => array('html')) ),
 			'wordwrap' => new Twig_Filter_Method( $this, 'wordwrapFilter'),
-
-			// 'jsonDecode' => new Twig_Filter_Method( $this, 'jsonDecodeFilter' ) 
 		);
 	}
 
@@ -44,38 +42,55 @@ class SproutEmailTwigExtension extends Twig_Extension
 	}
 	
 	// @TODO - This doesn't work properly yet, but we need to 
-	// get Email to support tags like this.  Site source of code, or update 
-	// to own use case: https://github.com/fabpot/Twig-extensions/blob/master/lib/Twig/Extensions/Extension/Text.php
+	// get Email to support tags like this.  
+	// 
+	// Wordwrap filter used the following Twig Extension as a starting point but has updated the 
+	// filter to chop out <p> tags and <br> tags, add \n breaks, and to not cut words off when
+	// wrapping lines.  Lines will wrap at the closest breaking space before the character limit.
+	// 
+	// https://github.com/fabpot/Twig-extensions/blob/master/lib/Twig/Extensions/Extension/Text.php
 	//
 	// {% wordwrap %}
 	// {% endwordwrap %}
 	//
 	public function wordwrapFilter($value, $length = 60, $separator = "\n", $preserve = false)
 	{
-		$sentences = array ();
+		$lines = array();
+
+		$value = str_replace("<p>", "", $value);
+		$value = str_replace("</p>", "\n\n", $value);
+		$value = preg_replace("/<br\W*?\/>/", "\n", $value);
+
+		if (substr($value, -2) == "\n\n") 
+		{
+			$value = substr($value, 0, -2);
+		}
 		
 		$previous = mb_regex_encoding();
 		mb_regex_encoding( craft()->templates->getTwig()->getCharset() );
 		
 		$pieces = mb_split( $separator, $value );
 		mb_regex_encoding( $previous );
-		
+
 		foreach ( $pieces as $piece )
 		{
 			while ( ! $preserve && mb_strlen( $piece, craft()->templates->getTwig()->getCharset() ) > $length )
 			{
-				$sentences [] = mb_substr( $piece, 0, $length, craft()->templates->getTwig()->getCharset() );
+				$count = count($lines);
+				$lines[] = mb_substr( $piece, 0, $length, craft()->templates->getTwig()->getCharset() );
+				
+				$lastSpacePosition = strrpos($lines[$count], ' ', 0);
+				$finalCharacters = trim(substr($lines[$count], $lastSpacePosition, 60));
+				$lines[$count] = substr($lines[$count], 0, $lastSpacePosition);
+				
+				$piece = $finalCharacters . $piece;
 				$piece = mb_substr( $piece, $length, 2048, craft()->templates->getTwig()->getCharset() );
+				
 			}
 			
-			$sentences [] = $piece;
+			$lines[] = $piece;
 		}
 		
-		return implode( $separator, $sentences );
+		return implode( $separator, $lines );
 	}
-	
-	// public function jsonDecodeFilter($string)
-	// {
-	// 	return json_decode( $string );
-	// }
 }
