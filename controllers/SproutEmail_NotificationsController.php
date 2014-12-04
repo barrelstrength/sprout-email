ea2a9d2744769412da3c2e13dc748d841cbdf4a2
Selvin Ortiz <selvin@selvin.co>
2014-12-04 11:28:21 -0600

    Notifications - Fixes issue where slugs were not auto generated on entry pages - Fixes issue where notifications were saved as entries and not campaigns

--- controllers/SproutEmail_NotificationsController.php
+++ controllers/SproutEmail_NotificationsController.php
@@ -5,58 +5,59 @@ namespace Craft;
  * Notifications controller
  */
 class SproutEmail_NotificationsController extends SproutEmail_CampaignController
-{	
+{
 	/**
 	 * Trigger notification
 	 */
 	public function actionTrigger()
 	{
-		if ( ! $id = craft()->request->getSegment( 5 ) )
+		if (!$id = craft()->request->getSegment(5))
 		{
-			die( 'Invalid request' );
+			die('Invalid request');
 		}
-		
-		$parts = explode( '-', $id );
-		$campaignId = array_shift( $parts );
-		
+
+		$parts      = explode('-', $id);
+		$campaignId = array_shift($parts);
+
 		// get the notification
-		$campaignNotification = craft()->sproutEmail_notifications->getCampaignNotificationByCampaignId( $id );
-		
+		$campaignNotification = craft()->sproutEmail_notifications->getCampaignNotificationByCampaignId($id);
+
 		// authenticate
-		if( ! $campaignNotification->options || ! isset($campaignNotification->options['options']['cronHash']))
+		if (!$campaignNotification->options || !isset($campaignNotification->options['options']['cronHash']))
 		{
 			die('Invalid Request');
 		}
 
-		if ( $campaignNotification->options['options']['cronHash'] != $id || $campaignNotification->notificationEvent->event != 'cron' )
+		if ($campaignNotification->options['options']['cronHash'] != $id || $campaignNotification->notificationEvent->event != 'cron')
 		{
-			die( 'Invalid request' );
+			die('Invalid request');
 		}
-		
+
 		// get campaign and send
 		$campaign = craft()->sproutEmail_campaign->getCampaignById($id);
-		$service = 'sproutEmail_' . lcfirst( $campaign->emailProvider );
-		craft()->{$service}->sendEntry( $campaign );
-		
-		exit( 0 );
+		$service  = 'sproutEmail_'.lcfirst($campaign->emailProvider);
+
+		craft()->{$service}->sendEntry($campaign);
+
+		exit(0);
 	}
 
 
 	public function actionNotificationSettingsTemplate(array $variables = array())
 	{
-		if (isset($variables['campaignId'])) 
+		if (isset($variables['campaignId']))
 		{
 			// If campaign already exists, we're returning an error object
-			if ( ! isset($variables['campaign']) ) 
+			if (!isset($variables['campaign']))
 			{
 				$variables['campaign'] = craft()->sproutEmail_campaign->getCampaignById($variables['campaignId']);
 			}
 		}
 		else
-		{	
+		{
 			$variables['campaign'] = new SproutEmail_Campaign();
 		}
-		
+
 		// Load our template
 		$this->renderTemplate('sproutemail/settings/notifications/_edit', $variables);
 	}
