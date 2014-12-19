85c988740529b5e46c18c5cac26ed0b0eae67671
Selvin Ortiz <selvin@selvin.co>
2014-12-19 09:03:18 -0600

    Adds the BaseMailer class to build a new API around.

---
+++ contracts/SproutEmailBaseMailer.php
@@ -0,0 +1,139 @@
+<?php
+namespace Craft;
+
+class SproutEmailBaseMailer
+{
+	/**
+	 * The settings for this mailer, stored in sproutemail_mailers
+	 * @var array
+	 */
+	protected $settings;
+
+	/**
+	 * Whether this mailer has been initialized
+	 * @var bool
+	 */
+	protected $initialized = false;
+
+	public function init()
+	{
+		$this->settings		= sproutEmail()->mailers->getSettingsByMailerName($this->getId());
+		$this->initialized	= true;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isInitialized()
+	{
+		return $this->initialized;
+	}
+
+	/**
+	 * Returns the mailer title when used in string context
+	 *
+	 * @return string
+	 */
+	public function __toString()
+	{
+		return $this->getTitle();
+	}
+
+	/**
+	 * Returns the service id to use as the formal identifier for all mailers
+	 *
+	 * @example sproutemail
+	 *
+	 * @return string
+	 */
+	final public function getId()
+	{
+		return preg_replace('/[^a-z]+/i', '', strtolower($this->getName()));
+	}
+
+	/**
+	 * Returns the qualified service name
+	 *
+	 * @todo Consider removing once all templates are using proper settings handling
+	 *
+	 * @example SproutEmail
+	 *
+	 * @return string
+	 */
+	public function getName() {}
+
+	/**
+	 * Returns the service title to use when displaying a label or similar use case
+	 *
+	 * @example Sprout Email Service
+	 *
+	 * @return string
+	 */
+	public function getTitle() {}
+
+	/**
+	 * Returns a short description of this mailer
+	 *
+	 * @example The Sprout Email services uses the Craft API to send emails
+	 *
+	 * @return string
+	 */
+	public function getDescription() {}
+
+	/**
+	 * Returns the settings for this mailer
+	 *
+	 * @return array
+	 */
+	public function getSettings()
+	{
+		if (!$this->isInitialized())
+		{
+			$this->init();
+		}
+
+		return $this->settings;
+	}
+
+	/**
+	 * Returns the settings and default values that should be stored when first installed
+	 *
+	 * @return array
+	 */
+	public function getDefaultSettings()
+	{
+		return array();
+	}
+
+	/**
+	 * Returns a rendered html string to use for capturing settings
+	 *
+	 * @return string
+	 */
+	public function getSettingsHtml() {}
+
+	/**
+	 * Returns the value that should be saved to settings for this mailer
+	 *
+	 * @example
+	 * return craft()->request->getPost('sproutemail');
+	 *
+	 * @return mixed
+	 */
+	public function prepareSettings()
+	{
+		return craft()->request->getPost($this->getId());
+	}
+
+	/**
+	 * Gives the service a chance to attach the value to the right field id before outputting it
+	 *
+	 * @param $value
+	 *
+	 * @return mixed
+	 */
+	public function prepareValue($value)
+	{
+		return $value;
+	}
+}
