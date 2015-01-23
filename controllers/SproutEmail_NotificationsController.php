a089e27df4bf2ce8fe31e074f47a7de8d6291ede
Selvin Ortiz <selvin@selvin.co>
2015-01-23 06:35:49 -0600

    Removes abomination actionTrigger() with die() & exist() like is the 90's

--- controllers/SproutEmail_NotificationsController.php
+++ controllers/SproutEmail_NotificationsController.php
@@ -2,51 +2,23 @@
 namespace Craft;
 
 /**
- * Notifications controller
+ * Class SproutEmail_NotificationsController
+ *
+ * @package Craft
  */
 class SproutEmail_NotificationsController extends SproutEmail_CampaignController
 {
 	/**
-	 * Trigger notification
+	 * Renders the notification settings template with passed in route variables
+	 *
+	 * @param array $variables
+	 *
+	 * @throws HttpException
 	 */
-	public function actionTrigger()
-	{
-		if (!$id = craft()->request->getSegment(5))
-		{
-			die('Invalid request');
-		}
-
-		$parts      = explode('-', $id);
-		$campaignId = array_shift($parts);
-
-		// get the notification
-		$campaignNotification = craft()->sproutEmail_notifications->getCampaignNotificationByCampaignId($id);
-
-		// authenticate
-		if (!$campaignNotification->options || !isset($campaignNotification->options['options']['cronHash']))
-		{
-			die('Invalid Request');
-		}
-
-		if ($campaignNotification->options['options']['cronHash'] != $id || $campaignNotification->notificationEvent->event != 'cron')
-		{
-			die('Invalid request');
-		}
-
-		// get campaign and send
-		$campaign = craft()->sproutEmail_campaign->getCampaignById($id);
-		$service  = 'sproutEmail_'.lcfirst($campaign->emailProvider);
-
-		craft()->{$service}->sendEntry($campaign);
-
-		exit(0);
-	}
-
 	public function actionNotificationSettingsTemplate(array $variables = array())
 	{
 		if (isset($variables['campaignId']))
 		{
-			// If campaign already exists, we're returning an error object
 			if (!isset($variables['campaign']))
 			{
 				$variables['campaign'] = sproutEmail()->campaigns->getCampaignById($variables['campaignId']);
@@ -57,9 +29,8 @@ class SproutEmail_NotificationsController extends SproutEmail_CampaignController
 			$variables['campaign'] = new SproutEmail_Campaign();
 		}
 
-		$variables['isMailerInstalled'] =  (bool) sproutEmail()->mailers->getMailerByName('coremailer');
+		$variables['isMailerInstalled'] =  (bool) sproutEmail()->mailers->isInstalled('coremailer');
 
-		// Load our template
 		$this->renderTemplate('sproutemail/settings/notifications/_edit', $variables);
 	}
 }
