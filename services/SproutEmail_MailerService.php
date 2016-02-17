8fd2610ee31e4f329c121499b4e4557c0a6d2a00
Dale Ramirez <dalefirstpage@gmail.com>
2016-02-18 02:23:44 +0800

    Added unit test for the checkboxField value and remove twig logic and changed it to mailer service method in Entries save notification. #721538

--- services/SproutEmail_MailerService.php
+++ services/SproutEmail_MailerService.php
@@ -583,4 +583,24 @@ class SproutEmail_MailerService extends BaseApplicationComponent
 
 		return false;
 	}
+	
+	public function getCheckboxFieldValue($options)
+	{
+		$value = '*';
+
+		if(isset($options))
+		{
+			if($options == '')
+			{
+				// Uncheck all checkboxes
+				$value = 'x';
+			}
+			else
+			{
+				$value = $options;
+			}
+		}
+
+		return $value;
+	}
 }
