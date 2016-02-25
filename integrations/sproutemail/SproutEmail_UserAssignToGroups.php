80f95a0cd78d075a8c4a29d9aa462a21f481dc47
Dale Ramirez <dalefirstpage@gmail.com>
2016-02-25 17:48:26 +0800

    Added support for user object, old and new group name on sprout email templates. #240538

--- integrations/sproutemail/SproutEmail_UserAssignToGroups.php
+++ integrations/sproutemail/SproutEmail_UserAssignToGroups.php
@@ -20,6 +20,44 @@ class SproutEmail_UserAssignToGroups extends SproutEmailBaseEvent
 		return Craft::t('Triggered when a user is assigned to a group.');
 	}
 
+	public function prepareParams(Event $event)
+	{
+		$values = array();
+		$userId  = $event->params['userId'];
+
+		$user = craft()->users->getUserById($userId);
+
+		$values['value']['user'] = $user;
+
+		$groups = craft()->userGroups->getGroupsByUserId($userId);
+
+		$oldNames = array();
+		if(!empty($groups))
+		{
+			foreach($groups as $group)
+			{
+				$oldNames[] = $group->name;
+			}
+		}
+
+		$groupIds = $event->params['groupIds'];
+		$newNames = array();
+		if(!empty($groupIds))
+		{
+			foreach($groupIds as $id)
+			{
+				$groupModel = craft()->userGroups->getGroupById($id);
+				$newNames[] = $groupModel->name;
+			}
+		}
+
+		$values['value']['oldgroups'] = $oldNames;
+		$values['value']['newgroups'] = $newNames;
+		$values['value']['groupIds']  = $groupIds;
+
+		return $values;
+	}
+
 	public function prepareOptions()
 	{
 		$assignedGroups = craft()->request->getPost('assignedGroups');
@@ -45,9 +83,10 @@ class SproutEmail_UserAssignToGroups extends SproutEmailBaseEvent
 		return craft()->templates->render('sproutemail/_events/userAssignGroups', $context);
 	}
 
-	public function validateOptions($options, $order, array $params = array())
+	public function validateOptions($options, $values, array $params = array())
 	{
-		$userId = $params['userId'];
+
+		$userId = $values['user']->id;
 
 		$groups = craft()->userGroups->getGroupsByUserId($userId);
 
@@ -61,7 +100,7 @@ class SproutEmail_UserAssignToGroups extends SproutEmailBaseEvent
 			}
 		}
 
-		$newGroups = $params['groupIds'];
+		$newGroups = $values['groupIds'];
 
 		$optionOld = $options['assignedGroups']['old'];
 		$optionNew = $options['assignedGroups']['new'];
