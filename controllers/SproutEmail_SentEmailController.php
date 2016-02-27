95a909d705ba8c5033d7f219af417e23681113e2
Ben Parizek <ben@barrelstrengthdesign.com>
2016-02-27 09:55:15 -0800

    Refactors SproutEmail_SentEmailController::actionGetViewContentModal
    - Updates $variables array to be defined all together in the render method signature
    - Updates conditional logic to remove need for additional else statements
    - Removes fallback that set body => htmlBody in some circumstances
    - Updates $body conditional to return null if no body is present (instead of false)

--- controllers/SproutEmail_SentEmailController.php
+++ controllers/SproutEmail_SentEmailController.php
@@ -13,42 +13,34 @@ class SproutEmail_SentEmailController extends BaseController
 		$this->requirePostRequest();
 		$this->requireAjaxRequest();
 
-		$variables = array();
 		$entryId = craft()->request->getRequiredPost('entryId');
-
 		$entry = sproutEmail()->sentemails->getSentEmailById($entryId);
-		$variables['entry'] = $entry;
+
+		$htmlBody = '';
+		$body = (!empty($entry->body)) ? $entry->body : null;
 
 		if (!empty($entry->htmlBody))
 		{
 			// Get only the body content
 			preg_match("/<body[^>]*>(.*?)<\/body>/is", $entry->htmlBody, $matches);
+
 			if (!empty($matches))
 			{
-				$htmlBody = $matches[1];
 				$string = trim(preg_replace('/\s+/', ' ', $matches[1]));
 				$htmlBody = $string;
 			}
-			else
-			{
-				$entry->body = $entry->htmlBody;
-				$htmlBody = '';
-			}
 		}
-		else
-		{
-			$htmlBody = '';
-		}
-		$body = (!empty($entry->body)) ? $entry->body : false;
 
-		$variables['body'] = $body;
-		$variables['htmlBody'] = $htmlBody;
-
-		$output = craft()->templates->render('sproutemail/sentemails/_view', $variables);
+		$content = craft()->templates->render('sproutemail/sentemails/_view', array(
+			'entry'    => $entry,
+			'body'     => $body,
+			'htmlBody' => $htmlBody
+		));
 
 		$response = new SproutEmail_ResponseModel();
-		$response->content = $output;
+		$response->content = $content;
 		$response->success = true;
+
 		$this->returnJson($response->getAttributes());
 	}
 }
