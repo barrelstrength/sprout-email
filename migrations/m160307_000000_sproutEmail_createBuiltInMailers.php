1c170b730afa6942bbebd3b6402dd1ec2a02c92c
Dale Ramirez <dalefirstpage@gmail.com>
2016-03-05 13:49:56 +0800

    Set mailers migration to remove process of install mailer button. #729997

---
+++ migrations/m160307_000000_sproutEmail_createBuiltInMailers.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Craft;
+
+
+class m160307_000000_sproutEmail_createBuiltInMailers extends BaseMigration
+{
+	/**
+	 * @return bool
+	 */
+	public function safeUp()
+	{
+
+		if($table = craft()->db->schema->getTable('{{sproutemail_mailers}}'))
+		{
+			$names = craft()->db->createCommand()
+				->select('name')
+				->from('sproutemail_mailers')
+				->queryAll();
+
+			$existingMailers = array();
+
+			if (!empty($names))
+			{
+				foreach ($names as $name)
+				{
+					$existingMailers[] = $name['name'];
+				}
+			}
+
+			$settings = array();
+
+			$settings['copypaste']['model'] = "Craft\\Model";
+
+			$settings['mailchimp']['apiKey']    = null;
+			$settings['mailchimp']['inlineCss'] = 1;
+			$settings['mailchimp']['model']     = "Craft\\Model";
+
+			$settings['campaignmonitor']['clientId'] = null;
+			$settings['campaignmonitor']['apiKey']   = null;
+			$settings['campaignmonitor']['model']    = "Craft\\Model";
+
+			foreach($settings as $id => $setting)
+			{
+				if(!in_array($id, $existingMailers))
+				{
+					craft()->db->createCommand()->insert('sproutemail_mailers',
+						array(
+							'name'     => $id,
+							'settings' => JsonHelper::encode($setting)
+						)
+					);
+				}
+			}
+		}
+
+		return true;
+	}
+}
