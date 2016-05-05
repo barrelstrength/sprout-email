4fde8e9cfbb30fd1d842bb23c2eff4023dcf060b
Dale Ramirez <dalefirstpage@gmail.com>
2016-05-05 14:09:50 +0800

    Add code snippet that converts html entity special characters to avoid error on rendering field

--- services/SproutEmail_DefaultMailerService.php
+++ services/SproutEmail_DefaultMailerService.php
@@ -824,6 +824,10 @@ class SproutEmail_DefaultMailerService extends BaseApplicationComponent
 
 		$htmlBody = $this->addPlaceholderStyleTags($email->htmlBody, $styleTags);
 
+		// Convert html entity special charcters to avoid error on rendering field
+		$email->body = htmlspecialchars_decode($email->body);
+		$htmlBody    = htmlspecialchars_decode($email->htmlBody);
+
 		// Process the results of the template s once more, to render any dynamic objects used in fields
 		$email->body     = sproutEmail()->renderObjectTemplateSafely($email->body, $object);
 		$email->htmlBody = sproutEmail()->renderObjectTemplateSafely($htmlBody, $object);
