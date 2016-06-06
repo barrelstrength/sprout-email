93e65460f740a804c2d526fa8ca2c52c9cd8dbe5
Dale Ramirez <dalefirstpage@gmail.com>
2016-06-06 23:59:33 +0800

    Added populate model method that generates campaign model on the fly when adding notification entry. Added integration for sprout import on sprout email entry notification type.

--- integrations/sproutimport/SproutEmail_EntrySproutImportElementImporter.php
+++ integrations/sproutimport/SproutEmail_EntrySproutImportElementImporter.php
@@ -13,6 +13,47 @@ class SproutEmail_EntrySproutImportElementImporter extends BaseSproutImportEleme
 		return new $model;
 	}
 
+	public function populateModel($model, $settings)
+	{
+		if ($settings['type'] == "notification")
+		{
+			$campaign = new SproutEmail_CampaignModel();
+
+			$campaign->mailer = "defaultmailer";
+		}
+		elseif (isset($settings['campaignId']))
+		{
+			$campaign = sproutEmail()->campaigns->getCampaignById($settings['campaignId']);
+		}
+		else
+		{
+			throw new \Exception("Specify a campaign id.");
+		}
+
+		$campaign->setAttributes($settings);
+
+		if (isset($settings['fieldLayout']) && !empty($settings['fieldLayout']))
+		{
+			foreach ($settings['fieldLayout'] as $name => $fields)
+			{
+				$entryFields = sproutImport()->getFieldIdsByHandle($name, $fields);
+
+				if (!empty($entryFields))
+				{
+					$fieldLayout = craft()->fields->assembleLayout($entryFields);
+					$fieldLayout->type = 'SproutEmail_Campaign';
+					$campaign->setFieldLayout($fieldLayout);
+				}
+			}
+		}
+
+		$this->campaignModel  = sproutEmail()->campaigns->saveCampaign($campaign);
+
+		$model->campaignId = $this->campaignModel->id;
+
+		$this->model = $model;
+	}
+
 	/**
 	 * @return bool
 	 * @throws Exception
@@ -20,6 +61,20 @@ class SproutEmail_EntrySproutImportElementImporter extends BaseSproutImportEleme
 	 */
 	public function save()
 	{
-		return sproutEmail()->entries->saveEntry($this->model);
+		$campaign = $this->campaignModel;
+
+		return sproutEmail()->entries->saveEntry($this->model, $campaign);
+	}
+
+	public function defineKeys()
+	{
+		// This will accept all attributes for campaign model and fieldLayout to pass validation
+		$campaigModel = new SproutEmail_CampaignModel;
+
+		$attributes = $campaigModel->defineAttributes();
+
+		$attributeKeys = array_keys($attributes);
+
+		return array_merge(array("fieldLayout"), $attributeKeys);
 	}
 }
