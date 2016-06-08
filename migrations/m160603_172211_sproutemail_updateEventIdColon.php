78664bd57f6864426ca4a319a868227217756250
Andre Lopez <andre@barrelstrengthdesign.com>
2016-06-08 13:11:18 -0400

    Fixes issue on migration when the eventId is renamed .

--- migrations/m160603_172211_sproutemail_updateEventIdColon.php
+++ migrations/m160603_172211_sproutemail_updateEventIdColon.php
@@ -13,33 +13,60 @@ class m160603_172211_sproutemail_updateEventIdColon extends BaseMigration
 	 */
 	public function safeUp()
 	{
-		$tableName = "sproutemail_campaigns_notifications";
+		$tableName  = 'sproutemail_campaigns_notifications';
+		$columnName = 'eventId';
+		$supportedEvents = array(
+			'entries-saveEntry',
+			'entries-deleteEntry',
+			'userSession-login',
+			'users-saveUser',
+			'users-deleteUser',
+			'users-activateUser',
+			'commerce_orders-onOrderComplete',
+			'commerce_transactions-onSaveTransaction',
+			'commerce_orderHistories-onStatusChange'
+		);
 
-		if ($table = $this->dbConnection->schema->getTable('{{' . $tableName . '}}'))
+		if (craft()->db->tableExists($tableName))
 		{
-			if ($table->getColumn('eventId') != null)
+			if (craft()->db->columnExists($tableName, $columnName))
 			{
 				$notifications = craft()->db->createCommand()
 					->select('id, eventId')
 					->from($tableName)
 					->queryAll();
 
-				if ($count = count($notifications))
+				foreach ($notifications as $notification)
 				{
-					foreach ($notifications as $notification)
-					{
-						$oldEventId = $notification['eventId'];
-						$newEventId = str_replace(":", "-", $oldEventId);
+					$oldEventId = $notification['eventId'];
+					$newEventId = str_replace(':', '-', $oldEventId);
 
-						craft()->db->createCommand()->update($tableName, array(
-							'eventId' => $newEventId ),
-							'id= :id',
-							array(
-								':id' => $notification['id']
-							)
-						);
+					if (in_array($newEventId, $supportedEvents))
+					{
+						$newEventId = 'SproutEmail-'.$newEventId;
 					}
+					else
+					{
+						$prefix = explode('-', $newEventId);
+
+						if (count($prefix) > 0)
+						{
+							$newEventId = ucfirst($prefix[0]).'-'.$newEventId;
+						}
+					}
+
+					craft()->db->createCommand()->update($tableName, array(
+						'eventId' => $newEventId ),
+						'id= :id',
+						array(
+							':id' => $notification['id']
+						)
+					);
 				}
+			}
+			else
+			{
+				SproutEmailPlugin::log("Column `$columnName` does not exists in the `$tableName` table.", LogLevel::Info, true);
 			}
 		}
 
