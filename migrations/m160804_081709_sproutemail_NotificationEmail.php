543d9febe170672f698c92ac52a043922bb7c8da
Dale Ramirez <dalefirstpage@gmail.com>
2016-08-05 12:19:29 +0800

    Created notificatin email migration class.

---
+++ migrations/m160804_081709_sproutemail_NotificationEmail.php
@@ -0,0 +1,19 @@
+<?php
+namespace Craft;
+
+/**
+ * The class name is the UTC timestamp in the format of mYYMMDD_HHMMSS_pluginHandle_migrationName
+ */
+class m160804_081709_sproutemail_NotificationEmail extends BaseMigration
+{
+	/**
+	 * Any migration code in here is wrapped inside of a transaction.
+	 *
+	 * @return bool
+	 */
+	public function safeUp()
+	{
+		SproutEmailPlugin::log('Running notification migration');
+		return true;
+	}
+}
