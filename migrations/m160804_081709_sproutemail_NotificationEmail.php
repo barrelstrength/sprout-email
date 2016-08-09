f0f7d0037c09f88b489fca16799a58251ed3c36a
Dale Ramirez <dalefirstpage@gmail.com>
2016-08-09 22:44:47 +0800

    Created query that gets old notification data such as campaign notification settings and notification entry. Also gets the recipient list to be moved to the notification recipient list table. Added query migration that inserts old notification to the new notification table that support the notificationEmail element type. Refactor the migration class and made the script as private methods for creating, moving and altering tables and columns.

--- migrations/m160804_081709_sproutemail_NotificationEmail.php
+++ migrations/m160804_081709_sproutemail_NotificationEmail.php
@@ -13,7 +13,87 @@ class m160804_081709_sproutemail_NotificationEmail extends BaseMigration
 	 */
 	public function safeUp()
 	{
-/*		$rows = craft()->db->createCommand()
+		$this->createNotificationTables();
+
+		$this->moveOldNotifications();
+
+		//$this->alterTablesColumns();
+
+		SproutEmailPlugin::log('Running naming convention migration');
+
+		return true;
+	}
+
+	private function createNotificationTables()
+	{
+		$tableName = 'sproutemail_notifications';
+
+		if (!craft()->db->tableExists($tableName))
+		{
+			try
+			{
+				// Create the craft_sproutemail_notifications table
+				craft()->db->createCommand()->createTable($tableName, array(
+					'id'                    => array('column' => 'integer', 'required' => true),
+					'name'                  => array('required' => true),
+					'template'              => array('required' => true),
+					'eventId'               => array('column' => 'text'),
+					'options'               => array('column' => 'text'),
+					'subjectLine'           => array('required' => true),
+					'recipients'            => array('required' => false),
+					'fromName'              => array('required' => false),
+					'fromEmail'             => array('required' => false),
+					'replyToEmail'          => array('required' => false),
+					'sent'                  => array('maxLength' => 1,
+					                                 'default' => false,
+					                                 'required' => true,
+					                                 'column' => 'tinyint',
+					                                 'unsigned' => true),
+					'enableFileAttachments' => array('maxLength' => 1,
+					                                 'default' => false,
+					                                 'required' => true,
+					                                 'column' => 'tinyint',
+					                                 'unsigned' => true),
+					'dateCreated'           => array('column' => 'datetime'),
+					'dateUpdated'           => array('column' => 'datetime'),
+					'fieldLayoutId'         => array('maxLength' => 11,
+					                                 'decimals' => 0,
+					                                 'unsigned' => false,
+					                                 'length' => 11,
+					                                 'column' => 'integer'),
+				), null, false);
+
+				craft()->db->createCommand()->createIndex($tableName, 'id');
+
+				// Add foreign keys to craft_sproutemail_notifications
+				craft()->db->createCommand()->addForeignKey($tableName, 'id', 'elements', 'id', 'CASCADE', null);
+				craft()->db->createCommand()->addForeignKey($tableName, 'fieldLayoutId', 'fieldlayouts',
+					'id', 'SET NULL', null);
+			}
+			catch (\Exception $e)
+			{
+				Craft::dd($e->getMessage(), 10, false);
+			}
+		}
+
+		$tableName = 'sproutemail_notification_recipientlists';
+
+		if (!craft()->db->tableExists($tableName))
+		{
+			// Create the craft_sproutemail_notification_recipientlists table
+			craft()->db->createCommand()->createTable($tableName, array(
+				'notificationId' => array('column' => 'integer', 'required' => true),
+				'list'           => array(),
+			), null, true);
+
+			// Add foreign keys to craft_sproutemail_notification_recipientlists
+			craft()->db->createCommand()->addForeignKey($tableName, 'notificationId', 'sproutemail_notifications', 'id', 'CASCADE', null);
+		}
+	}
+
+	private function moveOldNotifications()
+	{
+		$entries = craft()->db->createCommand()
 			->select('*')
 			->from('sproutemail_campaigns setting')
 			->join('sproutemail_campaigns_notifications notification', 'setting.id = notification.campaignId')
@@ -22,19 +102,59 @@ class m160804_081709_sproutemail_NotificationEmail extends BaseMigration
 			->queryAll();
 
 		$oldNotifications = array();
-		foreach ($rows as $key => $row)
+		foreach ($entries as $key => $entry)
 		{
 			$recipientList = craft()->db->createCommand()
 				->select('*')
 				->from('sproutemail_campaigns_entries_recipientlists')
-				->where(array('type' => 'notification', 'entryId' => $row['id']))
+				->where(array('type' => 'notification', 'entryId' => $entry['id']))
 				->queryAll();
 
-			$oldNotifications[$key] = $row;
+			$oldNotifications[$key] = $entry;
 
 			$oldNotifications[$key]['recipientList'] = $recipientList;
+		}
 
-		}*/
+		if (!empty($oldNotifications))
+		{
+			foreach ($oldNotifications as $oldNotification)
+			{
+				$insertData = array(
+					'id'            => $oldNotification['id'],
+					'name'          => $oldNotification['name'],
+					'template'      => $oldNotification['template'],
+					'eventId'       => $oldNotification['eventId'],
+					'options'       => $oldNotification['options'],
+					'subjectLine'   => $oldNotification['subjectLine'],
+					'recipients'    => $oldNotification['recipients'],
+					'fromName'      => $oldNotification['fromName'],
+					'fromEmail'     => $oldNotification['fromEmail'],
+					'replyToEmail'  => $oldNotification['replyToEmail'],
+					'sent'          => $oldNotification['sent'],
+					'fieldLayoutId' => $oldNotification['fieldLayoutId'],
+					'enableFileAttachments' => $oldNotification['enableFileAttachments'],
+				);
+
+				craft()->db->createCommand()->insert('sproutemail_notifications', $insertData);
+
+				craft()->db->createCommand()->update('elements', array(
+					'type' => 'SproutEmail_NotificationEmail'
+				),
+					'id= :id', array(':id' => $oldNotification['id'])
+				);
+
+				//if (!empty($oldNotification['recipientList']))
+				//{
+				//	craft()->db->createCommand()->insert('sproutemail_notification_recipientlists', array(
+				//		'' => ''
+				//	));
+				//}
+			}
+		}
+	}
+
+	private function alterTablesColumns()
+	{
 		$tableName  = "sproutemail_campaigns";
 		$columnName = "type";
 
@@ -57,74 +177,5 @@ class m160804_081709_sproutemail_NotificationEmail extends BaseMigration
 		{
 			craft()->db->createCommand()->renameTable($tableName, 'sproutemail_campaigns');
 		}
-
-		$tableName = 'sproutemail_notifications';
-
-		if (!craft()->db->tableExists($tableName))
-		{
-			try
-			{
-
-			// Create the craft_sproutemail_notifications table
-			craft()->db->createCommand()->createTable($tableName, array(
-				'id'                    => array('column' => 'integer', 'required' => true, 'primaryKey' => true),
-				'name'                  => array('required' => true),
-				'template'              => array('required' => true),
-				'eventId'               => array('column' => 'text'),
-				'options'               => array('column' => 'text'),
-				'subjectLine'           => array('required' => true),
-				'recipients'            => array('required' => false),
-				'fromName'              => array('required' => false),
-				'fromEmail'             => array('required' => false),
-				'replyToEmail'          => array('required' => false),
-				'sent'                  => array('maxLength' => 1,
-				                                 'default' => false,
-				                                 'required' => true,
-				                                 'column' => 'tinyint',
-				                                 'unsigned' => true),
-				'enableFileAttachments' => array('maxLength' => 1,
-				                                 'default' => false,
-				                                 'required' => true,
-				                                 'column' => 'tinyint',
-				                                 'unsigned' => true),
-				'dateCreated'           => array('column' => 'datetime'),
-				'dateUpdated'           => array('column' => 'datetime'),
-				'fieldLayoutId'         => array('maxLength' => 11,
-				                                 'decimals' => 0,
-				                                 'unsigned' => false,
-				                                 'length' => 11,
-				                                 'column' => 'integer'),
-			), null, false);
-
-			craft()->db->createCommand()->createIndex($tableName, 'id');
-
-			// Add foreign keys to craft_sproutemail_notifications
-			craft()->db->createCommand()->addForeignKey($tableName, 'id', 'elements', 'id', 'CASCADE', null);
-			craft()->db->createCommand()->addForeignKey($tableName, 'fieldLayoutId', 'fieldlayouts',
-				'id', 'SET NULL', null);
-			}
-			catch (\Exception $e)
-			{
-				Craft::dd($e->getMessage(), 10, false);
-			}
-		}
-
-/*		$tableName = 'sproutemail_notification_recipientlists';
-
-		if (!craft()->db->tableExists($tableName))
-		{
-			// Create the craft_sproutemail_notification_recipientlists table
-			craft()->db->createCommand()->createTable($tableName, array(
-				'notificationId' => array('column' => 'integer', 'required' => true),
-				'list'           => array(),
-			), null, true);
-
-			// Add foreign keys to craft_sproutemail_notification_recipientlists
-			craft()->db->createCommand()->addForeignKey($tableName, 'notificationId', 'sproutemail_notifications', 'id', 'CASCADE', null);
-		}*/
-
-		SproutEmailPlugin::log('!Running naming convention migration');
-
-		return true;
 	}
 }
