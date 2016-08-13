786fe67d53bd94334fbf66c13d34d2fee16bc852
Ben Parizek <ben@barrelstrengthdesign.com>
2016-08-13 16:09:37 -0700

    Updates Campaign Email migration foreign key handling

--- migrations/m160804_000005_sproutEmail_migrateCampaignEmails.php
+++ migrations/m160804_000005_sproutEmail_migrateCampaignEmails.php
@@ -13,13 +13,27 @@ class m160804_000005_sproutEmail_migrateCampaignEmails extends BaseMigration
 	 */
 	public function safeUp()
 	{
-		$tableName = "sproutemail_campaigns_entries";
+		$oldTableName = "sproutemail_campaigns_entries";
+		$newTableName = "sproutemail_campaigns";
 
-		if (craft()->db->tableExists($tableName) && !craft()->db->tableExists('sproutemail_campaigns'))
+		if (craft()->db->tableExists($oldTableName) && !craft()->db->tableExists($newTableName))
 		{
 			SproutEmailPlugin::log('Rename sproutemail_campaigns_entries table to sproutemail_campaigns');
 
-			craft()->db->createCommand()->renameTable($tableName, 'sproutemail_campaigns');
+			$oldTable = MigrationHelper::getTable($oldTableName);
+
+			// Solve issue on older version of MySQL where we can't rename columns with a FK
+			MigrationHelper::dropAllForeignKeysOnTable($oldTable);
+
+			craft()->db->createCommand()->renameTable($oldTableName, $newTableName);
+
+			$newTable = MigrationHelper::getTable($newTableName);
+
+			// Drop the FK again, since renameTable restores fks
+			MigrationHelper::dropAllForeignKeysOnTable($newTable);
+
+			craft()->db->createCommand()->addForeignKey($newTableName, 'id', 'elements', 'id', 'CASCADE');
+			craft()->db->createCommand()->addForeignKey($newTableName, 'campaignId', 'sproutemail_campaigntype', 'id', 'CASCADE');
 		}
 
 		return true;
