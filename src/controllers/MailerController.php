4722e4d243c8f15c6c0d74736a287654e340b8d7
Dale Ramirez <dalefirstpage@gmail.com>
2018-07-20 21:39:00 +0800

    - Removed unused edit settings template - Added User Login class event for notifications

--- src/controllers/MailerController.php
+++ src/controllers/MailerController.php
@@ -10,37 +10,6 @@ use Craft;
 class MailerController extends Controller
 {
     /**
-     * Renders the Mailer Edit template
-     *
-     * @param array $variables
-     *
-     * @throws \HttpException
-     */
-    public function actionEditSettingsTemplate(array $variables = [])
-    {
-        $mailerId = $variables['mailerId'] ?? null;
-
-        if (!$mailerId) {
-            throw new \HttpException(404, Craft::t('sprout-email', 'No mailer id was provided'));
-        }
-
-        $mailer = SproutBase::$app->mailers->getMailerByName($mailerId);
-
-        if (!$mailer) {
-            throw new \HttpException(404, Craft::t('sprout-email', 'No mailer was found with that id'));
-        }
-
-        if (!$settings) {
-            $settings = $mailer->getSettings();
-        }
-
-        $this->renderTemplate('sprout-base-email/settings/mailers/edit', [
-            'mailer' => $mailer,
-            'settings' => $settings
-        ]);
-    }
-
-    /**
      * Provides a way for mailers to render content to perform actions inside a a modal window
      *
      * @return \yii\web\Response
---
+++ src/events/notificationevents/UsersLogin.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace barrelstrength\sproutemail\events\notificationevents;
+
+use barrelstrength\sproutbase\app\email\base\NotificationEvent;
+use craft\events\UserEvent;
+use craft\records\User as UserRecord;
+use Craft;
+use yii\web\User;
+
+
+/**
+ * @property UserEvent $event
+ */
+class UsersLogin extends NotificationEvent
+{
+    /**
+     * @inheritdoc
+     */
+    public function getEventClassName()
+    {
+        return User::class;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getEventName()
+    {
+        return User::EVENT_AFTER_LOGIN;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getEventHandlerClassName()
+    {
+        return UserEvent::class;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getName()
+    {
+        return Craft::t('sprout-email', 'When a user is logged in.');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getDescription()
+    {
+        return Craft::t('sprout-email', 'Triggered when a user is logged in.');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getEventObject()
+    {
+        return $this->event->user;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getMockEventObject()
+    {
+        $criteria = UserRecord::find();
+
+        return $criteria->one();
+    }
+}
