b2b9e03cd72d1aa1007597953c13bb1ce1055cb0
Ben Parizek <ben@barrelstrengthdesign.com>
2018-11-14 06:49:07 -0500

    Fixes logic bug in User Save Notification Event

--- src/events/notificationevents/UsersSave.php
+++ src/events/notificationevents/UsersSave.php
@@ -220,8 +220,8 @@ class UsersSave extends NotificationEvent
 
     private function isValidUserGroupIds($currentUsersUserGroups)
     {
-        return count($this->userGroupIds) > 0
-            AND ($this->userGroupIds !== '*')
+        return $this->userGroupIds !== '*'
+            AND count($this->userGroupIds) > 0
             AND count($currentUsersUserGroups);
     }
 
