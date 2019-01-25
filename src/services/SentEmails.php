a7dec0842203a180d280af351baed370eb9b9d9e
Andre L <andrelopezd@gmail.com>
2019-01-25 15:21:47 -0400

    Fixes xdebug nested error.

--- src/services/SentEmails.php
+++ src/services/SentEmails.php
@@ -230,7 +230,7 @@ class SentEmails extends Component
         // Remove deliveryStatus as we can determine it from the status in the future
         $infoTable = $infoTable->getAttributes();
         unset($infoTable['deliveryStatus']);
-        $sentEmail->info = Json::encode($infoTable);
+        $sentEmail->info = json_encode($infoTable);
 
         try {
             if (Craft::$app->getElements()->saveElement($sentEmail)) {
